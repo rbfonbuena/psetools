6ac56e0c152b3bd1fb766157963a637874b0023b
Romano Fonbuena <30283493+rbfonbuena@users.noreply.github.com>
2018-03-12 20:26:00 +0800

    Minor fixes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,11 +6,11 @@
       ad-style="display:block;width:320px;height:100px;margin:1rem auto;"
       ad-format="none">
     </AdSense>
-    <div class="disclaimer">
+    <div class="subfooter">
       <h3>DISCLAIMER</h3>
       <p class="disclaimer">None of the tools and information contained on this site should be used or considered as financial advise or recommendations to buy securities and investments. Please consult with your licensed broker before making important financial decisions. The user assumes all risk associated with all the information taken from this website. PSETools shall not be liable for any damages and loss of profits as a result of using this website and/or any websites linked to this site.</p>
+      <p class="copyright">&copy; 2018 PSETools</p>
     </div>
-    <p class="copyright">&copy; 2018 PSETools</p>
   </div>
 </template>
 
@@ -34,27 +34,26 @@ export default {
   max-width: 640px;
   margin: 0 auto;
   text-align: center;
-  opacity: 0.75;
-
-  p {
-    font-size: 14px;
-    line-height: 1.5;
-  }
 
   a {
     color: inherit;
     border-bottom: 1px dotted;
   }
 }
-.disclaimer {
+.subfooter {
+  opacity: 0.75;
+
   h3 {
     margin-bottom: 5px;
     font-size: 14px;
   }
-
-  p {
+  .disclaimer {
     margin-top: 0;
     font-size: 0.8rem;
+  }
+  .copyright {
+    font-size: 14px;
+    line-height: 1.5;
   }
 }
 </style>
