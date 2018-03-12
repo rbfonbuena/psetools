c3ccedd372da9388a6543765a154446b69b5e48f
Romano Fonbuena <30283493+rbfonbuena@users.noreply.github.com>
2018-03-12 20:01:26 +0800

    AdSense component

---
+++ src/components/AdSense.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ins class="adsbygoogle"
+      :data-ad-client="adClient"
+      :data-ad-slot="adSlot"
+      :data-ad-format="adFormat"
+      :style="adStyle"></ins>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdSense',
+  props: {
+    adClient: {
+      type: String,
+      required: true
+    },
+    adSlot: {
+      type: String,
+      required: true
+    },
+    adFormat: {
+      type: String,
+      required: false,
+      default: 'auto'
+    },
+    adStyle: {
+      type: String,
+      required: false,
+      default: 'display: block'
+    }
+  },
+  data () {
+    return {}
+  },
+  mounted () {
+    (window.adsbygoogle = window.adsbygoogle || []).push({})
+  }
+}
+</script>
